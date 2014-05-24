0ccb10de086290da4b8f39de4917d60d9e2a6154
bfhuang <bfhuang@thoughtworks.com>
2014-05-24 13:13:16 +0800

    add the personclient to call the soap service provided by personService by WebServiceTemplate

--- personClient/src/main/java/PersonClient.java
+++ personClient/src/main/java/PersonClient.java
@@ -1,4 +1,3 @@
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.ws.client.core.WebServiceTemplate;
@@ -10,12 +9,12 @@ public class PersonClient {
 	public static void main(String[] strings) {
 
 
-		ApplicationContext context = new FileSystemXmlApplicationContext("/src/resources/applicationContext.xml");
+		ApplicationContext context = new FileSystemXmlApplicationContext("classpath:applicationContext.xml");
 		WebServiceTemplate webServiceTemplate = (WebServiceTemplate) context.getBean("webServiceTemplate");
 
 		GetPersonRequest request = new GetPersonRequest();
-		GetPersonResponse response = new GetPersonResponse();
+		GetPersonResponse response = (GetPersonResponse) webServiceTemplate.marshalSendAndReceive(request);
 
-
+		System.out.println(response.getPerson().size());
 	}
 }
